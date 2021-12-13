480f820193b056b339336e838af64f05ace84c66
Adriano Rama <ramaadriano@outlook.com>
2021-12-13 10:28:26 +0100

    commit

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
+
+
 const state = {
 
 }
 
 function renderHeader() {
-
+    const headerEl = document.createElement(`header`)
+    const h2El = document.createElement(`h2`)
+    h2El.textContent = `Hollixton`
+    
+    document.body.append(headerEl)
+    headerEl.append(h2El)
 }
 
 function renderMain() {
+    const mainEl = document.createElement(`main`)
+    const h2El = document.createElement(`h2`)
+    
+    document.body.append(mainEl)
+    mainEl.append(h2El)
 
 }
 
 function renderFooter() {
-
+    const footerEl = document.createElement(`footer`)
+    const h2El = document.createElement(`h2`)
+    
+    document.body.append(footerEl)
+    footerEl.append(h2El)
 }
 
 function render() {
@@ -21,3 +37,5 @@ function render() {
     renderMain()
     renderFooter()
 }
+
+render()
