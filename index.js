ed1958ec80806994b9a8a824850e707afdcb2bf3
Adriano Rama <ramaadriano@outlook.com>
2021-12-13 10:14:35 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const state = {
+
+}
+
+function renderHeader() {
+
+}
+
+function renderMain() {
+
+}
+
+function renderFooter() {
+
+}
+
+function render() {
+    document.body.innerHTML = ``
+
+    renderHeader()
+    renderMain()
+    renderFooter()
+}
